e174ac2787b1a9254109df3647f92fa7bcb4ce54
Muma Mukuka <vipar123@gmail.com>
2020-08-27 18:58:01 +0200

    added referral intents

--- app/app/src/main/java/moh/org/zm/smarthealthcommunity/activity/stableoncare/ReferralActivity.java
+++ app/app/src/main/java/moh/org/zm/smarthealthcommunity/activity/stableoncare/ReferralActivity.java
@@ -1,8 +1,10 @@
 package moh.org.zm.smarthealthcommunity.activity.stableoncare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,18 @@ import moh.org.zm.smarthealthcommunity.R;
 import moh.org.zm.smarthealthcommunity.helpers.AppDatabase;
 import moh.org.zm.smarthealthcommunity.helpers.AppExecutors;
 import moh.org.zm.smarthealthcommunity.models.StableOnCare;
+
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.DP_FREQUENCY;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.DP_QUANTITY;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.DRUG_PRODUCT;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.HH_COUGH;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.HH_FEVER;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.HH_HEADACHE;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.HH_WEIGHT_LOSS;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.PE_BLOOD_PRESSURE;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.PE_TEMPERATURE;
+import static moh.org.zm.smarthealthcommunity.helpers.Constants.PE_URINE_PROTEIN;
+
 
 public class ReferralActivity extends AppCompatActivity {
 
@@ -31,25 +45,27 @@ public class ReferralActivity extends AppCompatActivity {
         final RadioGroup refHTCAppointment = findViewById(R.id.refHTCAppointment);
         Button saveRecord = findViewById(R.id.btnSave);
 
+        final Intent intent = getIntent();
+
         saveRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 /*   final StableOnCare stableOnCare = new StableOnCare(
+                    final StableOnCare stableOnCare = new StableOnCare(
                     UUID.randomUUID().toString(),
                         "",
                         "",
-                        getSelectedRadioValue(fever.getCheckedRadioButtonId()),
-                        getSelectedRadioValue(cough.getCheckedRadioButtonId()),
-                        getSelectedRadioValue(weightloss.getCheckedRadioButtonId()),
-                        getSelectedRadioValue(headache.getCheckedRadioButtonId()),
-                        bloodpressure.getText().toString(),
-                        temperature.getText().toString(),
-                        "",
-                        getSelectedRadioValue(clientdemonstrated.getCheckedRadioButtonId()),
-                        getSelectedRadioValue(askedchallenges.getCheckedRadioButtonId()),
-                        quantity.getText().toString(),
-                        "",
-                        "",
+                        intent.getStringExtra(HH_FEVER),
+                            intent.getStringExtra(HH_COUGH),
+                            intent.getStringExtra(HH_WEIGHT_LOSS),
+                            intent.getStringExtra(HH_HEADACHE),
+                            intent.getStringExtra(PE_BLOOD_PRESSURE),
+                            intent.getStringExtra(PE_TEMPERATURE),
+                            intent.getStringExtra(PE_URINE_PROTEIN),
+                            "",
+                            "",
+                            intent.getStringExtra(DRUG_PRODUCT),
+                            intent.getStringExtra(DP_QUANTITY),
+                            intent.getStringExtra(DP_FREQUENCY),
                         "",
                         getSelectedRadioValue(referralconfirm.getCheckedRadioButtonId()),
                         getSelectedRadioValue(refmalecircumcision.getCheckedRadioButtonId()),
@@ -57,7 +73,7 @@ public class ReferralActivity extends AppCompatActivity {
                         getSelectedRadioValue(refPartnerForART.getCheckedRadioButtonId()),
                         getSelectedRadioValue(refFP.getCheckedRadioButtonId()),
                         getSelectedRadioValue(refHTCAppointment.getCheckedRadioButtonId()));
-*/
+
 
                AppExecutors.getInstance().diskIO().execute(new Runnable() {
                     @Override
@@ -72,6 +88,14 @@ public class ReferralActivity extends AppCompatActivity {
                         //finish();
                     }
                 });
+            }
+
+            public String getSelectedRadioValue(int id) {
+                RadioButton selectedRadioButton = findViewById(id);
+                if (selectedRadioButton != null) {
+                    return selectedRadioButton.getText().toString();
+                }
+                return "";
             }
         });
     }
